44b5940526babbc659e580c004874ce7ba193413
KavitejVakte <kavitejvakte12@gmail.com>
2026-08-23 21:36:22 +0530

    added all code

---
+++ Variables/Boolean.java
@@ -0,0 +1,8 @@
+public class Boolean{
+    public static void main(String[] args){
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        boolean isOnline = true;
+        System.out.println(isJavaFun + " " + isFishTasty + " " + isOnline);
+    }
+}
