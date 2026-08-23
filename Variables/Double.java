e2769b3447d9916756ed6d409f261f5b060fdfa9
KavitejVakte <kavitejvakte12@gmail.com>
2026-08-23 21:12:43 +0530

    added the new file

---
+++ Variables/Double.java
@@ -0,0 +1,11 @@
+public class Double{
+    public static void main(String[] args){
+        double price = 10.99;
+        double gpa = 3.75;
+        double temperature = 98.6;
+        System.out.println("$" + price +"" + gpa + "" + temperature);
+        
+
+
+    }
+}
