3102d897068e40786c7c3310cdb23bab4860c9e8
KavitejVakte <kavitejvakte12@gmail.com>
2026-08-23 20:52:34 +0530

    added all the code

---
+++ frist_program/main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello,Kavitej welcome to Java");
+    }
+}
+
+// always remember that String name as S captial and System also have  S capital
+
+
